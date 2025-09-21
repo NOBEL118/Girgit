f82890961d7bf91db4db29010b35ba84f79c63d1
NOBEL VANSH <nobelvansh118@gmail.com>
2025-09-21 16:31:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,28 +3,28 @@ function randomColor(){
         let color = "#" ;
         for(let i = 0; i < 6 ; i++){
             color += hex[Math.floor(Math.random()*16)] ;
-        }
+        };
         return color 
-    }
-    const body = document.querySelector(".main") ;
-    const startBtn = document.querySelector("#start") ;
-    const stopBtn = document.querySelector("#stop") ;
-    let intervalId ;
+    };
 
-    const startChange = function () {
+const body = document.querySelector(".main") ;
+const startBtn = document.querySelector("#start") ;
+const stopBtn = document.querySelector("#stop") ;
+let intervalId ;
+
+const startChange = function () {
         function bodyColor(){
             body.style.backgroundColor = randomColor() ;
-        }
+        };
         if (!intervalId){
-        intervalId = setInterval(bodyColor , 1000) 
-        }
-        
-    }
+        intervalId = setInterval(bodyColor , 1000); 
+        };
+    };
 
-    const stopChange = function (){
-        clearInterval(intervalId)
-        intervalId = null ;
-    }
+const stopChange = function (){
+    clearInterval(intervalId);
+    intervalId = null ;
+};
 
-    startBtn.addEventListener("click", startChange )
-    stopBtn.addEventListener("click" , stopChange )
+startBtn.addEventListener("click", startChange )
+stopBtn.addEventListener("click" , stopChange )
